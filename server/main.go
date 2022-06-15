4f15a8075d093ef333d0d446882f3f8ecdd108e1
daniel <pinkdaniel@web.de>
2022-06-15 16:13:39 +0200

    one route on backend

--- server/main.go
+++ server/main.go
@@ -9,25 +9,11 @@ import (
 
 var addr = flag.String("addr", ":7777", "http service address")
 
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	http.ServeFile(w, r, "home.html")
-}
-
 func main() {
 	flag.Parse()
 	hub := root.NewHub(10)
 	go hub.Run()
-	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		root.ServeWs(hub, w, r)
 	})
 	err := http.ListenAndServe(*addr, nil)
