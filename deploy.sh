9a11740373b2dda235bf0095b3e7e23feb6940b9
daniel <pinkdaniel@web.de>
2022-06-18 14:06:08 +0200

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,3 @@ ssh ansible@gymcadia.com 'cd /opt/game && /opt/game/update.sh'
 cd frontend
 yarn build
 scp -r dist/* ansible@gymcadia.com:/opt/game/frontend/
-scp -r assets ansible@gymcadia.com:/opt/game/frontend/
