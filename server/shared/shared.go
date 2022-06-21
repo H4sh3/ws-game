3cdd8e98dfd703742691c7b8924d45061aee8be0
daniel <pinkdaniel@web.de>
2022-06-21 18:10:47 +0200

    add copy vector func

--- server/shared/shared.go
+++ server/shared/shared.go
@@ -19,3 +19,7 @@ func (v1 *Vector) Dist(v2 *Vector) float64 {
 	b := math.Pow(float64(v1.Y-v2.Y), 2)
 	return math.Sqrt(a + b)
 }
+
+func (v1 *Vector) Copy() Vector {
+	return Vector{X: v1.X, Y: v1.Y}
+}
