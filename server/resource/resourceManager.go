328e20593206e3316ec87be26786e099f14ca6b3
daniel <pinkdaniel@web.de>
2022-06-21 13:16:57 +0200

    add get,set & delete to ResourceManager

--- server/resource/resourceManager.go
+++ server/resource/resourceManager.go
@@ -1,19 +1,40 @@
 package resource
 
+import (
+	"errors"
+)
+
 type ResourceManager struct {
-	Resources map[int]*Resource
+	resources map[int]*Resource
 	idCnt     int
+}
+
+func (gm *ResourceManager) GetResource(id int) (*Resource, error) {
+	r := gm.resources[id]
+	if r == nil {
+		return r, errors.New("resource does not exist")
+	} else {
+		return r, nil
+	}
+}
+
+func (gm *ResourceManager) SetResource(r *Resource) {
+	gm.resources[r.Id] = r
+}
+
+func (gm *ResourceManager) DeleteResource(id int) {
+	delete(gm.resources, id)
 }
 
 func NewResourceManager() *ResourceManager {
 	return &ResourceManager{
-		Resources: make(map[int]*Resource),
+		resources: make(map[int]*Resource),
 		idCnt:     0,
 	}
 }
 
 func (rm *ResourceManager) Add(r *Resource) {
-	rm.Resources[r.Id] = r
+	rm.resources[r.Id] = r
 }
 
 func (rm *ResourceManager) GetResourceId() int {
