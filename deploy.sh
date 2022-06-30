a4d15091c9ae6c0f59b1bcd3526e094ceee41097
h4sh3 <pinkdaniel@web.de>
2022-06-30 16:24:48 +0200

    restart nginx after deploy -> clear cache

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# build backend
 ssh $PROD 'cd /opt/game && /opt/game/update.sh'
 
+# build and deploy frontend
 cd frontend
 yarn build
 scp -r dist/* $PROD:/opt/game/frontend/
+
+# restart nginx for cache clear
+ssh -t $PROD 'sudo /bin/systemctl restart nginx'
+
+echo "All done"
